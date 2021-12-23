2f46a868b22a959fee1713ac4a87ea2242c3f08c
snakehead007 <snakehead007@protonmail.com>
2021-12-23 11:06:09 +0100

    intial commit

---
+++ index.md
@@ -0,0 +1,3 @@
+## test startpagina
+
+[ici](ici)
---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+require 'vendor/autoload.php';
+use ParsedownExtra;
+error_log("reading file " . $_REQUEST['p']);
+
+function readMD($file) {
+    //reads only Markdown files
+    if (substr($file, -3) != '.md') {
+        return;
+    }
+    //get text from file
+    $text = file_get_contents($file);
+    //parse text
+    $Extra = new ParsedownExtra();
+    $html = $Extra->text($text);
+    //get title
+    $title = $Extra->line(substr($text, 0, strpos($text, "\n")));
+    // remove any number of # from title
+    $title = preg_replace('/^#+/', '', $title);
+    //return title and html
+    return array($title, $html);
+}
+
+
+list($title,$body) = readMD($_REQUEST['p']??"index.md");
+echo HTML(
+    HEAD($title).
+    BODY(
+        $body
+    )
+);
+
+function BODY($children){
+    return '<body>
+    '.$children.'
+    </body>';
+}
+
+function HTML($children){
+    return  '
+    <html lang="en">
+    '.$children.'
+    </html>';
+    
+}
+function HEAD($title){
+    return '
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>'.$title.'</title>
+    <link rel="stylesheet" href="https://ilt.kuleuven.be/php72/ici/docs/stylesheet.css">
+</head>';
+}
