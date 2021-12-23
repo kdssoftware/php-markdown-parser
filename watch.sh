ea278723ca8c87e69297ec4c1512796f23e602fd
Karel <snakehead007@pm.me>
2021-12-23 11:29:13 +0100

    update

--- watch.sh
+++ watch.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 watch -n 1 "git pull && git add ./* && git stage ./* &&git commit -m 'update' && git push"
+#RewriteRule ^https:\/\/ilt\.kuleuven\.be\/php72\/docs\/(.*)$ https://ilt.kuleuven.be/php72/docs/?p=$1 [R=301,NC,L]
